0270ffe3c1f7b3b2fa50221f9744cfccb96bd5e2
Sourabh singh <96938361+SouSingh@users.noreply.github.com>
2024-06-16 18:34:22 +0530

    Update Crud.py

--- Crud.py
+++ Crud.py
@@ -55,7 +55,7 @@ tables = [table[0] for table in cursor.fetchall()]
 
 # Streamlit app
 st.sidebar.title("Select Table")
-selected_table = st.sidebar.selectbox("Table", ' '.join(word.capitalize() for word in tables.split('_')))
+selected_table = st.sidebar.selectbox("Table", ' '.join(word.split('_').capitalize() for word in tables))
 
 # CRUD operations based on the selected table
 if selected_table:
